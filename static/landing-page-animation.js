378f14e0e277034184fadc8843cbe11ad3dd8c94
Garabed96 <garonazarian09@gmail.com>
2023-01-25 10:07:25 +0700

    Landing Page styling improved 1.01

---
+++ static/landing-page-animation.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i) =>
+        setTimeout(() => (el.innerHTML += letter), 95 * i)
+    );
+    setInterval(() => typeWriter(el), 8000);
+}
+
+typeWriter(elementEl);
