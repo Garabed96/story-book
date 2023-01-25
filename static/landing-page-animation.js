955c6badf6b37a6a0008bb50a0ea50b5c885c2f0
Garabed96 <garonazarian09@gmail.com>
2023-01-25 12:33:01 +0700

    Landing Page styling improved 1.02 (Image bottom to top animation)

--- static/landing-page-animation.js
+++ static/landing-page-animation.js
@@ -1,12 +1,18 @@
 'use strict';
 
-function typeWriter(el) {
-    const textArray = el.innerHTML.split('');
-    el.innerHTML = '';
-    textArray.forEach((letter, i) =>
-        setTimeout(() => (el.innerHTML += letter), 95 * i)
-    );
-    setInterval(() => typeWriter(el), 8000);
+
+var tl = gsap.timeline();
+
+function animateElements() {
+    tl.from('.img_container', {y: 100, duration: 1, ease: "power2.outIn"})
+    .from('img', {scale: 1.3, opacity: 0, duration: 1, ease: "power2.outIn"}, "<");
 }
 
-typeWriter(elementEl);
+
+
+$(document).ready(function() {
+    $(".animate-name").addClass("show-image");
+     $(".animate-name").css({transform: 'scale(0.33) rotate(5deg)'});
+});
+
+
